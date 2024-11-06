dde6e68e3c7c289ef47732be67f1675cfed12b1e
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-11-06 11:35:00 +0300

    getAllProducts request implemented to controller

--- src/main/java/com/project/SpringEcom/controller/ProductController.java
+++ src/main/java/com/project/SpringEcom/controller/ProductController.java
@@ -1,18 +1,28 @@
 package com.project.SpringEcom.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.SpringEcom.model.Product;
+import com.project.SpringEcom.service.ProductService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/products")
-    public String getProducts() {
-        return "All Prducts Are Here!";
+    public List<Product> getProducts() {
+        return productService.getAllProducts();
     }
     
 
