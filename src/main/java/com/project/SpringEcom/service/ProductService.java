ac14c35f511e2555331aee26560a8898504321e7
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-11-06 11:34:41 +0300

    ProductService & getAllProducts request created

---
+++ src/main/java/com/project/SpringEcom/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.project.SpringEcom.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.SpringEcom.model.Product;
+import com.project.SpringEcom.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+}
