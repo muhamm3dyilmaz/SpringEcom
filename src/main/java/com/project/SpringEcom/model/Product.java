2694805c00a572bf5fc58a68d1c3d2de251ec130
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-11-07 19:17:25 +0300

    date format added

--- src/main/java/com/project/SpringEcom/model/Product.java
+++ src/main/java/com/project/SpringEcom/model/Product.java
@@ -3,6 +3,8 @@ package com.project.SpringEcom.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,8 +27,9 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yy")
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
-    
+
 }
