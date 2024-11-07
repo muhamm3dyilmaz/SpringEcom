75dacac17539700fb4209c332bd315add5649fbb
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-11-07 20:01:57 +0300

    addProduct fn added to service

--- src/main/java/com/project/SpringEcom/service/ProductService.java
+++ src/main/java/com/project/SpringEcom/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.project.SpringEcom.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.project.SpringEcom.model.Product;
 import com.project.SpringEcom.repository.ProductRepository;
@@ -20,6 +22,14 @@ public class ProductService {
 
     public Product getProductById(int id) {
         return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return productRepository.save(product);
     }
 
 }
