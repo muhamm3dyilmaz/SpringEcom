655165bef1379716b839c7619f782397e0b71c69
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-11-08 14:44:50 +0300

    deleteProduct service created

--- src/main/java/com/project/SpringEcom/service/ProductService.java
+++ src/main/java/com/project/SpringEcom/service/ProductService.java
@@ -32,4 +32,8 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    public void deleteProduct(int id) {
+        productRepository.deleteById(id);
+    }
+
 }
